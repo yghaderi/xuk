632fb8566fbd0478d1c0164c8bc03edacc4a7afc
yaghoub <y.ghaderi@outlook.com>
2023-11-10 05:41:08 +0330

    add trend doc on TA

---
+++ xuk/ta/trend/__init__.py
@@ -0,0 +1,1 @@
+from xuk.ta.trend import dow
--- xuk/ta/trend/dow.py
+++ xuk/ta/trend/dow.py
@@ -2,8 +2,10 @@ from scipy.signal import argrelextrema
 import numpy as np
 import polars as pl
 
+__all__ = ["dow"]
 
-def _peak_trough_index(price: np.array, length: int):
+
+def _peak_trough_index(close: np.array, length: int):
     """
     .. raw:: html
 
@@ -13,44 +15,73 @@ def _peak_trough_index(price: np.array, length: int):
 
     Parameters
     ---------
-    price: np.array
+    close: np.array
         آرایه‌یِ قیمت
-    order: int
+    length: int
         طولِ دوره‌هایِ محلی
 
     Returns
     -------
-    peak_idx: list
+    peak_idx: numpy.array
         اندیسِ قله‌ها
-    trough_idx
+    trough_idx: numpy.array
         اندیسِ درَه‌ها
     """
-    peak_idx = argrelextrema(price, np.greater_equal, order=length)[0]
-    trough_idx = argrelextrema(price, np.less_equal, order=length)[0]
+    peak_idx = argrelextrema(close, np.greater_equal, order=length)[0]
+    trough_idx = argrelextrema(close, np.less_equal, order=length)[0]
     return peak_idx, trough_idx
 
 
-def _separation_peak_trough(df):
+def _classification_peak_trough(df: pl.DataFrame):
     """
-    It compares the position of the peak and trough compared to the previous one.
+    .. raw:: html
+
+        <div dir="rtl">
+            موقعیتِ قله‌ها و درّه‌ها رو نسبت به هم مقایسه می‌کنه تا به HH, HL, LH, LL نشانه-گذاری بشن.
+        </div>
+
     In hh_lh where True indicate Higher High and False indicate Lower High.
     In ll_hl where True indicate Lower Low and False indicate Higher Low.
-    :return: hh_lh and ll_hl columns.
+
+    Parameters
+    ----------
+    df: polars.Dataframe
+
+    Returns
+    -------
+    polars.DataFrame
     """
     df_peak = df.filter(pl.col("peak").eq(1))
     df_trough = df.filter(pl.col("trough").eq(1))
     df_peak = df_peak.with_columns(
-        hh_lh=pl.col("price") > pl.col("price").shift(1)
+        hh_lh=pl.col("close") > pl.col("close").shift(1)
     ).select(["date", "symbol", "hh_lh"])
     df_trough = df_trough.with_columns(
-        ll_hl=pl.col("price") < pl.col("price").shift(1)
+        ll_hl=pl.col("close") < pl.col("close").shift(1)
     ).select(["date", "symbol", "ll_hl"])
     df = df.join(df_peak, on=["date", "symbol"], how="left")
     df = df.join(df_trough, on=["date", "symbol"], how="left")
     return df
 
 
-def _recognize_trend(hh_lh, ll_hl):
+def _recognize_trend(hh_lh: float, ll_hl: float):
+    """
+    .. raw:: html
+
+        <div dir="rtl">
+            بر مبنایِ HH, HL, LH, LL ها و رویکردِ داو، روند نوع روند رو تشخیص میده.
+        </div>
+
+    Parameters
+    ----------
+    hh_lh: float
+    ll_hl: float
+
+    Returns
+    -------
+    trend : int
+        1 به معنیِ روندِ صعودی، -1 روندِ نزولی و 0 بی-روند
+    """
     if hh_lh:
         if not ll_hl:
             return 1
@@ -62,11 +93,26 @@ def _recognize_trend(hh_lh, ll_hl):
     return 0
 
 
-def dow_trend(df: pl.DataFrame, length: int):
+def dow(df: pl.DataFrame, length: int):
     """
-    An uptrend is defined primarily as successively higher peaks and troughs.
-    A downtrend is defined as successively lower peaks and troughs.
-    :return: The trend column, where 1 indicates an uptrend, -1 a downtrend, and 0 a trend-less.
+    .. raw:: html
+
+        <div dir="rtl">
+            روندِ صعودی وختیه که HH, HL هایِ متوالی داشته باشیم. روندِ نزولی هم وختیه که LH, LL هایِ متوالی داشته باشیم.
+        </div>
+
+    The trend column, where 1 indicates an uptrend, -1 a downtrend, and 0 a trend-less.
+
+    Parameters
+    ----------
+    df: polars.DataFrame
+        شاملِ ستون‌هایِ date, symbol, close
+    length: int
+        طولِ دوره‌هایِ محلی
+
+    Returns
+    -------
+    polars.DataFrame
     """
     df_ = pl.DataFrame()
     for name, data in df.group_by("symbol"):
@@ -76,7 +122,7 @@ def dow_trend(df: pl.DataFrame, length: int):
             pl.lit(0).alias("trough"),
         )
         peak_idx, trough_idx = _peak_trough_index(
-            price=data["price"].to_numpy(), length=length
+            close=data["close"].to_numpy(), length=length
         )
         for i in peak_idx:
             data[int(i), "peak"] = 1
@@ -84,7 +130,7 @@ def dow_trend(df: pl.DataFrame, length: int):
             data[int(i), "trough"] = 1
         df_ = pl.concat([df_, data])
 
-    df = _separation_peak_trough(df_)
+    df = _classification_peak_trough(df_)
     df = df.with_columns(
         [
             pl.col("hh_lh")
